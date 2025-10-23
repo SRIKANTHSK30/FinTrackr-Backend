614d53911e760433b10f112b852b2df3491232f6
Goutham Balaji P S <26gouthamba@gmail.com>
2025-10-23 22:10:32 +0530

    needed packages installed

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,31 +3,28 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'module',
-    project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
-    '@typescript-eslint/recommended-requiring-type-checking',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
+    es2022: true,
+  },
+  globals: {
+    globalThis: 'readonly',
   },
   ignorePatterns: ['.eslintrc.js', 'dist/', 'node_modules/', 'coverage/'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/prefer-const': 'error',
-    '@typescript-eslint/no-var-requires': 'error',
     'prefer-const': 'error',
     'no-var': 'error',
     'no-console': 'warn',
     'no-debugger': 'error',
+    'no-useless-catch': 'off',
   },
 };
--- src/config/redis.ts
+++ src/config/redis.ts
@@ -6,14 +6,19 @@ const redisClient = createClient({
 });
 
 redisClient.on('error', (err) => {
+  // eslint-disable-next-line no-console
   console.error('Redis Client Error:', err);
 });
 
 redisClient.on('connect', () => {
+  // eslint-disable-next-line no-console
   console.log('Connected to Redis');
 });
 
 // Connect to Redis
-redisClient.connect().catch(console.error);
+redisClient.connect().catch((err) => {
+  // eslint-disable-next-line no-console
+  console.error(err);
+});
 
 export default redisClient;
