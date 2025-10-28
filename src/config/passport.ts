d05b1eb24778a859d117f1c6354e05253a7e8267
Goutham Balaji P S <26gouthamba@gmail.com>
2025-10-28 21:23:13 +0530

    Feat : Google Oauth is completed  and Database connected with backend

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -11,6 +11,7 @@ passport.use(
       clientID: env.GOOGLE_CLIENT_ID!,
       clientSecret: env.GOOGLE_CLIENT_SECRET!,
       callbackURL: '/api/v1/auth/google/callback',
+      scope: ['profile', 'email'],
     },
     async (_accessToken, _refreshToken, profile, done) => {
       try {
